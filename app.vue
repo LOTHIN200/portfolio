c4ddba187728ea1f28a89a739bb40902526edd01
Cristiano <lothin9999@gmail.com>
2024-12-10 13:55:55 +0700

    update

--- app.vue
+++ app.vue
@@ -3,3 +3,15 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<script setup lang="ts">
+useSeoMeta({
+  title: "DEV",
+  ogTitle: "Welcome to My Developer Portfolio",
+  description:
+    "Explore my personal portfolio to discover my web development projects, skills, and achievements. Let's build something amazing together.",
+  ogDescription:
+    "Check out my web development portfolio showcasing cutting-edge projects and technical expertise. Let's create something great together!",
+  ogImage: "./public/logo.png",
+  twitterCard: "LOTHIN",
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Dev",
+    },
+    
+  },
   ssr: true,
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
