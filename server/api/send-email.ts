3b58704614f089d2ee74fddbe0d4e42022f139de
Cristiano <lothin9999@gmail.com>
2024-12-24 15:29:39 +0700

    update

---
+++ server/api/send-email.ts
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: body.to,
+    subject: body.subject,
+    text: body.text,
+    html: body.html,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    return { success: true, info };
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return { success: false, error: error.message };
+    } else {
+      return { success: false, error: "An unknown error occurred" };
+    }
+  }
+});
