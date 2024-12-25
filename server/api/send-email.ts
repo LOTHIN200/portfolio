2c3d73150406eb8d008f51016bb23263a17313b4
Cristiano <lothin9999@gmail.com>
2024-12-25 09:48:13 +0700

    update

--- server/api/send-email.ts
+++ server/api/send-email.ts
@@ -6,14 +6,14 @@ export default defineEventHandler(async (event) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASS,
+      user: "lothin9999@gmail.com", //process.env.GMAIL_USER,
+      pass: "kjfc mtqa annp ldhc", //process.env.GMAIL_PASS,
     },
   });
 
   const mailOptions = {
-    from: process.env.GMAIL_USER,
-    to: body.to,
+    from: "lothin9999@gmail.com", //process.env.GMAIL_USER,
+    to: "lothininfo@gmail.com", //body.to,
     subject: body.subject,
     text: body.text,
     html: body.html,
