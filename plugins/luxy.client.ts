2c7f17e9b6ce47f8e93123575aba1b81c599e5d1
lothin9999 <lothin9999@gmail.com>
2025-01-22 10:08:30 +0700

    update

--- plugins/luxy.client.ts
+++ plugins/luxy.client.ts
@@ -1,24 +1,9 @@
-// import Luxy from "luxy.js";
-
-// export default defineNuxtPlugin((nuxtApp) => {
-//   nuxtApp.hook("app:mounted", () => {
-//     if (process.client) {
-//       setTimeout(() => {
-//         Luxy.init({
-//           wrapper: "#luxy",
-//           targets: ".luxy-el",
-//           wrapperSpeed: 0.08,
-//         });
-//       }, 100);
-//     }
-//   });
-// });
 import Luxy from "luxy.js";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook("app:mounted", () => {
-    if (process.client) {
-      const observer = new MutationObserver((mutations, obs) => {
+    if (nuxtApp.isClient) {
+      setTimeout(() => {
         const luxyElement = document.getElementById("luxy");
         if (luxyElement) {
           Luxy.init({
@@ -26,15 +11,8 @@ export default defineNuxtPlugin((nuxtApp) => {
             targets: ".luxy-el",
             wrapperSpeed: 0.08,
           });
-          obs.disconnect(); // Stop observing once the element is found
         }
-      });
-
-      // Start observing the document body for changes
-      observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-      });
+      }, 100);
     }
   });
 });
