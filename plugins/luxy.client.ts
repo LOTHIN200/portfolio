c7499b743961386bf94a3ec8cafe48283f51756c
lothin9999 <lothin9999@gmail.com>
2025-01-22 09:25:48 +0700

    update

--- plugins/luxy.client.ts
+++ plugins/luxy.client.ts
@@ -3,12 +3,12 @@ import { onMounted } from "vue";
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (import.meta.client) {
-    onMounted(() => {
-      Luxy.init({
-        wrapper: "#luxy",
-        targets: ".luxy-el",
-        wrapperSpeed: 0.1,
-      });
+    // onMounted(() => {
+    Luxy.init({
+      wrapper: "#luxy",
+      targets: ".luxy-el",
+      wrapperSpeed: 0.1,
+      // });
     });
   }
 });
