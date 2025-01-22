b1927b750d38fcd5a852f49470dfb283e32cc5f9
lothin9999 <lothin9999@gmail.com>
2025-01-22 09:59:33 +0700

    update

--- plugins/luxy.client.ts
+++ plugins/luxy.client.ts
@@ -3,11 +3,13 @@ import Luxy from "luxy.js";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook("app:mounted", () => {
     if (process.client) {
-      Luxy.init({
-        wrapper: "#luxy",
-        targets: ".luxy-el",
-        wrapperSpeed: 0.08,
-      });
+      setTimeout(() => {
+        Luxy.init({
+          wrapper: "#luxy",
+          targets: ".luxy-el",
+          wrapperSpeed: 0.08,
+        });
+      }, 100);
     }
   });
 });
