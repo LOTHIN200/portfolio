a6e6e8ac4be2a8b1ff74d835bd3870686af2fba8
lothin9999 <lothin9999@gmail.com>
2025-01-22 09:52:20 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
 import { SpeedInsights } from "@vercel/speed-insights/vue";
+import { onMounted, ref } from "vue";
+
+const isSpeedInsightsEnabled = ref(true);
+
+onMounted(() => {
+  const script = document.createElement("script");
+  script.src = "https://lothin-dev.vercel.app/_vercel/speed-insights/script.js";
+  script.onerror = () => {
+    isSpeedInsightsEnabled.value = false;
+  };
+  document.head.appendChild(script);
+});
 </script>
 
 <template>
   <div>
     <UtilityMouseCircle />
     <UtilityAppParticle />
-    <SpeedInsights v-if="process.client" />
+    <SpeedInsights v-if="isSpeedInsightsEnabled" />
     <slot />
   </div>
 </template>
